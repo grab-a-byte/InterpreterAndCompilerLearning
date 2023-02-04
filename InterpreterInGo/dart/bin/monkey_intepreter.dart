7d90636ff00026b8726a9be69e2682d856870e8d
parkeradam <30392071+parkeradam@users.noreply.github.com>
2023-02-04 23:05:37 +0000

    Init dart project to reimplement and solidify learning

---
+++ InterpreterInGo/dart/bin/monkey_intepreter.dart
@@ -0,0 +1,5 @@
+import 'package:monkey_intepreter/monkey_intepreter.dart' as monkey_intepreter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${monkey_intepreter.calculate()}!');
+}
---
+++ InterpreterInGo/dart/test/monkey_intepreter_test.dart
@@ -0,0 +1,8 @@
+import 'package:monkey_intepreter/monkey_intepreter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
