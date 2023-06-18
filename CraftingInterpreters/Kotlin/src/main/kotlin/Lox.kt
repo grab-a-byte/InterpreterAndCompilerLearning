99621a4521d13758cd6d91b8e9b32db298134a99
parkeradam <30392071+parkeradam@users.noreply.github.com>
2023-06-18 16:18:02 +0100

    Start Crafting Interpreters in Kotlin

---
+++ CraftingInterpreters/Kotlin/src/main/kotlin/Lox.kt
@@ -0,0 +1,39 @@
+import java.io.File
+import kotlin.system.exitProcess
+
+object Lox {
+
+    private var hadError: Boolean = false
+
+    private fun run(source: String){
+        val scanner = Scanner(source)
+        val tokens = scanner.scanTokens()
+        for (token in tokens) {
+            println(token)
+        }
+    }
+
+    fun runFile(file: String){
+        val bytes: ByteArray = File(file).readBytes()
+        run(bytes.toString())
+        if (hadError) exitProcess(65)
+    }
+
+    fun runPrompt(){
+        while (true) {
+            print("> ")
+            val line: String = readlnOrNull() ?: break
+            run(line)
+            hadError = false
+        }
+    }
+
+    fun error(line: Int, message: String) {
+        report(line, "", message)
+    }
+
+    private fun report(line: Int, where: String, message: String){
+        println("[line $line] Error $where : $message")
+        hadError = true
+    }
+}
---
+++ CraftingInterpreters/Kotlin/src/main/kotlin/Scanner.kt
@@ -0,0 +1,3 @@
+class Scanner(val input: String) {
+    fun scanTokens() : List<Token> {return listOf(Token())}
+}
