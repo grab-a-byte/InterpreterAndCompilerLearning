553da23b70fb11374ad6552a711f9bc3e136bcac
grab-a-byte <30392071+grab-a-byte@users.noreply.github.com>
2024-09-09 22:47:48 +0100

    more work from book

--- CraftingInterpreters/C/CraftingInterpreters/debug.c
+++ CraftingInterpreters/C/CraftingInterpreters/debug.c
@@ -9,9 +9,22 @@ void disassembleChunk(Chunk *chunk, const char *name) {
     }
 }
 
+static int simpleInstruction(const char *name, int offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
 int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
-    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset-1]) {
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
         printf("   | ");
     } else {
         printf("%4d ", chunk->lines[offset]);
@@ -23,25 +36,22 @@ int disassembleInstruction(Chunk *chunk, int offset) {
             return simpleInstruction("OP_RETURN", offset);
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_NEGATE:
+            return simpleInstruction("OP_NEGATE", offset);
+        case OP_ADD:
+            return simpleInstruction("OP_ADD", offset);
+        case OP_SUBSTRACT:
+            return simpleInstruction("OP_SUBTRACT", offset);
+        case OP_MULTIPLY:
+            return simpleInstruction("OP_MULTIPLY", offset);
+        case OP_DIVIDE:
+            return simpleInstruction("OP_DIVIDE", offset);
         default:
             printf("Unknown opcode %d", instruction);
             return offset + 1;
     }
 }
 
-int simpleInstruction(const char *name, int offset) {
-    printf("%s\n", name);
-    return offset + 1;
-}
-
 void printValue(Value value) {
     printf("%g", value);
-}
-
-int constantInstruction(const char *name, Chunk *chunk, int offset) {
-    uint8_t constant = chunk->code[offset + 1];
-    printf("%-16s %4d '", name, constant);
-    printValue(chunk->constants.values[constant]);
-    printf("'\n");
-    return offset + 2;
 }
